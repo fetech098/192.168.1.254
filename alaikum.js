491ff387cd53f8c71bc33b128024cf014577130b
fetech098 <134030077+fetech098@users.noreply.github.com>
2023-05-21 15:42:42 -0600

    Create alaikum.js

---
+++ alaikum.js
@@ -0,0 +1,15 @@
+Cloning into 'C:\Users\feter\Documents\GitHub\192.168.1.254'...
+remote: Enumerating objects: 106, done.        
+remote: Counting objects: 100% (106/106), done.        
+remote: Compressing objects: 100% (104/104), done.        
+remote: Total 106 (delta 38), reused 0 (delta 0), pack-reused 0        
+Receiving objects: 100% (106/106), 4.83 MiB | 1.78 MiB/s, done.
+Resolving deltas: 100% (38/38), done.
+error: invalid path 'hex:.js'
+fatal: unable to checkout working tree
+warning: Clone succeeded, but checkout failed.
+You can inspect what was checked out with 'git status'
+and retry with 'git restore --source= 
+alaikum226
+
+Would you like to retry cloning 
